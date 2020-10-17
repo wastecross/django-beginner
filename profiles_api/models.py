d4ba1e99e0c24dbca1df709a5edc1a251da0c395
wastecross <charly_1310@outlook.com>
2020-10-17 16:21:29 -0500

    feat: add hello api view

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -37,7 +37,7 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
-    is_staaff = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
 
     objects = UserProfileManager()
 
