842ece0fcfc76f71b6287cd32787b2bfa63f5be1
wastecross <charly_1310@outlook.com>
2020-10-17 20:44:43 -0500

    fix: fix profile feed

--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -10,3 +10,14 @@ class UpdateOwnProfile(permissions.BasePermission):
             return True
 
         return obj.id == request.user.id
+
+
+class UpdateOwnStatus(permissions.BasePermission):
+    """Allow users to update their own satus"""
+
+    def has_object_permissions(self, request, view, obj):
+        """Check the user is trying to update their own status"""
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.user_profile.id == request.user.id
