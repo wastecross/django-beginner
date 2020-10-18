11beab4c27d507137128d5668a519ac725843fb9
wastecross <charly_1310@outlook.com>
2020-10-17 21:37:12 -0500

    fix: remove line

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -4,7 +4,6 @@ set -e
 
 # TODO: Set to URL of git repo.
 PROJECT_GIT_URL='https://github.com/wastecross/django-beginner.git'
-
 PROJECT_BASE_PATH='/usr/local/apps/django-beginner'
 
 echo "Installing dependencies..."
